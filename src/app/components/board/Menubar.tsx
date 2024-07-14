53b3a3e2f093ede99a4ffe87dbcca6d50ad5605e
natitbc <nati@techbase.camp>
2024-07-14 18:37:14 +0700

    home | add searchbox

--- src/app/components/board/Menubar.tsx
+++ src/app/components/board/Menubar.tsx
@@ -5,12 +5,16 @@ import CategoryDropdown from "./CategoryDropdown";
 
 export default function Menubar() {
   return (
-    <div className="flex flex-row">
-      <Searchbox />
-      <CategoryDropdown />
-      <Button type="primary" html="a" link="/signin">
-        Create +
-      </Button>
+    <div className="flex flex-row justify-between gap-[20px]">
+      <div className="w-[535px]">
+        <Searchbox />
+      </div>
+      <div className="flex gap-4">
+        <CategoryDropdown />
+        <Button type="primary" html="a" link="/signin">
+          Create +
+        </Button>
+      </div>
     </div>
   );
 }
--- src/app/components/board/Searchbox.tsx
+++ src/app/components/board/Searchbox.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 export default function Searchbox() {
   return (
     <>
-      <input type="text" placeholder="Search" />
+      <div className="relative w-full border border-[#fff] bg-transparent py-2 px-5 rounded-md">
+        <Image
+          src="/img/icon-search.svg"
+          alt="search"
+          className="absolute top-1/2 -translate-y-1/2"
+          width={20}
+          height={20}
+        ></Image>
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-full bg-[transparent] ps-6 font-plex font-light"
+        />
+      </div>
     </>
-  )
+  );
 }
