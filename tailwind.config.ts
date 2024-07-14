31fafada37f882825358859d6615e9c3da85e04f
natitbc <nati@techbase.camp>
2024-07-14 16:18:05 +0700

    signin | set font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,10 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+      plex: ["IBM Plex Sans", "sans-serif"],
+    },
     colors: {
       "green-board": {
         "500": "#243831",
